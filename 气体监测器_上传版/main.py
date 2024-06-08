496881443746a4a23ba6499c4afcb639ca0e986d
Invasion <181967486@qq.com>
2024-06-09 05:22:53 +0800

    Add files via upload

---
+++ 气体监测器_上传版/main.py
@@ -0,0 +1,8 @@
+from 气体监测器_上传版 import Start
+data = {
+    "Name":"io1",
+    "气体触发值":150,
+    "传输地址":"http://你的IP/你的接口",
+    "间隔时间":3
+}
+Start(data, 80)
---
+++ 气体监测器_屏显版/main.py
@@ -0,0 +1,6 @@
+from 气体监测器_屏显版 import Start
+data = {
+    "气体触发值":150,
+    "间隔时间":3
+}
+Start(data, 80)
