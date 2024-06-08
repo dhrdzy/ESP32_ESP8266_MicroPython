2d9b6763d64ec93a2c4e7a22d941d87003feb8ba
Invasion <181967486@qq.com>
2024-06-08 08:02:16 +0800

    Add files via upload

--- 温度监测器_屏显上传版/main.py
+++ 温度监测器_屏显上传版/main.py
@@ -2,7 +2,7 @@ from 温度监测器_屏显上传版 import Start
 data = {
     "Name":"io1",
     "设备GPIO口":16,
-    "传输地址":"http://invasion.x3322.net:10004/ttt1",
+    "传输地址":"http://你的IP/你的接口",
     "间隔时间":3
 }
 Start(data, 80)
